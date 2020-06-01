2c8545f44c9a4b1b19d60694a97e3d2e69a2976d
prabin89 <prabindrasapkota89@gmail.com>
2020-06-01 20:05:32 +0545

    MVC in Action

---
+++ VidMVC/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidMVC.Models;
+
+namespace VidMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Sherlock" };
+
+            return View(movie);
+        }
+    }
+}
---
+++ VidMVC/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VidMVC.Models
+{
+    public class Movie
+    {
+        public String Name { get; set; }
+        public int Id { get; set; }
+    }
+}
