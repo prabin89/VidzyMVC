6159f67bd1a8da4de4ea5752475672ed970ac0ef
prabin89 <prabindrasapkota89@gmail.com>
2020-06-02 19:32:15 +0545

    Movie and customer 's Mvc created

---
+++ VidMVC/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidMVC.Models;
+using VidMVC.ViewModel;
+
+namespace VidMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Collection()
+        {
+            var customer = new List<Customer> {
+                new Customer{Name ="Jack Ma "},
+                new Customer{Name ="Elon Musk "}
+            };
+
+            var CustomList = new CustomerCollectionViewModel
+            {
+                Customers = customer
+
+            };
+            return View(CustomList);
+        }
+        [Route("Customer/Collection/{Id}")]
+        public ActionResult ByCustomerId(int Id)
+        {
+            return Content("Customer Id =" + Id);
+        }
+    }
+}
---
+++ VidMVC/ViewModel/CustomerCollectionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VidMVC.Models;
+
+namespace VidMVC.ViewModel
+{
+    public class CustomerCollectionViewModel
+    {
+        public IList<Customer> Customers { get; set; }
+    }
+}
